d8de5d8e9a98b92ec97a1cc66731a17341bd9d02
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-06-24 15:03:45 +0300

    feat: autotest

--- controllers/cards.js
+++ controllers/cards.js
@@ -30,7 +30,7 @@ const createCards = (req, res, next) => {
 
 const deleteCardsById = (req, res, next) => {
   try {
-    const { cardId } = req.params.cardId;
+    const { cardId } = req.body;
     const card = Card.findById(cardId).populate('owner');
     if (!card) {
       throw new NotFoundError('Card not found');
@@ -49,7 +49,7 @@ const deleteCardsById = (req, res, next) => {
 
 const likeCard = (req, res, next) => {
   try {
-    const { cardId } = req.params.cardId;
+    const { cardId } = req.body;
     const card = Card.findByIdAndUpdate(
       req.params.cardId,
       { $addToSet: { likes: cardId } },
