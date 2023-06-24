080c4a016f31a8413db2c84984d9f09da081c903
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-06-24 13:30:51 +0300

    feat: autotest

--- routes/users.js
+++ routes/users.js
@@ -14,23 +14,22 @@ router.get('/', getUsers);
 router.get('/:userId', celebrate({
   body: Joi.object().keys({
     id: Joi.string().alphanum().length(24),
-  }).unknown(true),
+  }),
 }), getUserById);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-  }).unknown(true),
+  }),
 }), resumeProfile);
 
 router.get('/me', resumeNowProfile);
 
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().regex(/https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+#?$/i,
-    ),
-  }).unknown(true),
+    avatar: Joi.string().regex(/https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+#?$/i),
+  }),
 }), resumeAvatar);
 
 module.exports = router;
