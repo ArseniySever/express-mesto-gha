1e8a60d17d9f446d955cbf27430e370f61e39bef
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-06-24 11:34:37 +0300

    feat: autotest

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,32 @@
 const jwt = require('jsonwebtoken');
-
-const { UnauthorizedError } = require('../error/UnauthorizedError');
+const { UnauthorizedError } = require('../errors');
 
 function auth(req, res, next) {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('UnregisteredUser');
-  }
-  const token = authorization.replace('Bearer ', '');
-  let payload;
   try {
-    payload = jwt.verify(token, 'secretcode');
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      throw new UnauthorizedError(
+        'Для выполнения действия необходима авторизация',
+      );
+    }
+
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+
+    try {
+      payload = jwt.verify(token, 'secretkey');
+    } catch (err) {
+      throw new UnauthorizedError(
+        'Для выполнения действия необходима авторизация',
+      );
+    }
+
+    req.user = payload;
+    next();
   } catch (err) {
-    throw new UnauthorizedError('UNAUTHORIZED');
+    next(err);
   }
-  req.user = payload;
-  next();
 }
 
 module.exports = { auth };
