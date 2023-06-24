9a706cb9e36a8b387ad0ca7de0b9874838c6df3d
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-06-24 21:59:42 +0300

    fix: ревью

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(imgConst),
+    avatar: Joi.string().regex({ imgConst }),
   }).unknown(true),
 }), createUser);
 
