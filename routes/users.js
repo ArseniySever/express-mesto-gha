2bee2d946c4eaf8cd701edd388f6d3a22735bf1d
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-06-25 09:36:14 +0300

    fix: ревью

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.get('/me', resumeNowProfile);
 
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(imgConst).required(),
+    avatar: Joi.string().pattern(imgConst),
   }),
 }), resumeAvatar);
 
--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-const imgConst = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/i;
+const imgConst = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/;
 
 module.exports = {
   imgConst,
