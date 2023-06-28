6d18035bf74cacfa47439f2ad62758c78db6bace
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-06-28 17:31:15 +0300

    fix:

--- app.js
+++ app.js
@@ -43,11 +43,11 @@ app.post('/signin', celebrate({
     password: Joi.string().required(),
   }).unknown(true),
 }), login);
+app.use(auth);
+app.use('/users', userRoutes);
+app.use('/cards', cardsRoutes);
 
-app.use('/users', auth, userRoutes);
-app.use('/cards', auth, cardsRoutes);
-
-app.use('/*', auth, () => {
+app.use('/*', () => {
   throw new NotFoundError('Inncorect link');
 });
 app.use(errors());
