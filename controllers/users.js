c115b7cdf97e8fb4fa9c31db47c7d80444688dde
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-06-29 17:14:51 +0300

    fix:

--- controllers/users.js
+++ controllers/users.js
@@ -19,7 +19,7 @@ const getUsers = (req, res, next) => {
 };
 
 const getUserById = (req, res, next) => {
-  const { id } = req.user._id;
+  const { id } = req.params.userId;
 
   User.findById(id)
     .then((user) => {
