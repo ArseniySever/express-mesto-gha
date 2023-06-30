988a76d445ba2bfa86e008af98b6e9ab5359aa80
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-06-30 17:07:08 +0300

    refactor: ошибки наносит ответный удар

--- controllers/users.js
+++ controllers/users.js
@@ -26,7 +26,7 @@ const getUserById = (req, res, next) => {
       if (!user) {
         throw new NotFoundError('User not found');
       }
-      return res.send({ user });
+      return res.send(user);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
